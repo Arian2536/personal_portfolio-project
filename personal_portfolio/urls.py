c67d6b6903ebf7478a36eafce10f80ae401e0ebc
Arian2536 <jobinromania@yahoo.com>
2024-09-03 15:22:48 +0200

    First commit!

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
@@ -24,7 +24,15 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('blog/', include('blog.urls')),
+
+    #path('', views.home, name='home'),
+    #path('blog/', include('blog.urls')),
 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+
+
+#
+
